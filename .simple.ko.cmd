b9e089c804ad9ae20e228c4f3b7735985de0cab9
prajjwaldangal <oscreader@OSC>
2018-02-06 05:34:29 -0700

    try again

--- .simple.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/oscreader/Documents/adv_os/project1/simple.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.16.0-4-common/scripts/module-common.lds --build-id  -o /home/oscreader/Documents/adv_os/project1/simple.ko /home/oscreader/Documents/adv_os/project1/simple.o /home/oscreader/Documents/adv_os/project1/simple.mod.o
